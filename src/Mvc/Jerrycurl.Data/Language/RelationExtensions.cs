ae98f306312cb446e4c773771efb0b10d37897f3
rhodosaur <hai@rsaur.me>
2020-11-23 01:04:56 +0100

    use As* syntax

--- src/Mvc/Jerrycurl.Data/Language/RelationExtensions.cs
+++ src/Mvc/Jerrycurl.Data/Language/RelationExtensions.cs
@@ -19,25 +19,25 @@ namespace Jerrycurl.Data.Language
         public static QueryBuffer AsBuffer(this ISchema schema, QueryType type)
             => new QueryBuffer(schema, type);
 
-        public static Query ToQuery(this IRelation relation, string queryText)
+        public static Query AsQuery(this IRelation relation, string queryText)
         {
             return new Query()
             {
                 QueryText = queryText,
-                Parameters = relation.ToParameters()
+                Parameters = relation.AsParameters()
             };
         }
 
-        public static Command ToCommand(this IRelation relation, string commandText)
+        public static Command AsCommand(this IRelation relation, string commandText)
         {
             return new Command()
             {
                 CommandText = commandText,
-                Parameters = relation.ToParameters()
+                Parameters = relation.AsParameters()
             };
         }
 
-        public static Command ToCommand(this IRelation relation, Func<IList<IParameter>, string> textBuilder)
+        public static Command AsCommand(this IRelation relation, Func<IList<IParameter>, string> textBuilder)
         {
             ParameterStore store = new ParameterStore();
 
@@ -64,19 +64,19 @@ namespace Jerrycurl.Data.Language
         public static DbDataReader As(this IRelation relation, params string[] header)
             => relation.GetDataReader(header);
 
-        public static IList<IParameter> ToParameters(this IField source, params string[] header)
-            => source.Select(header).ToParameters();
+        public static IList<IParameter> AsParameters(this IField source, params string[] header)
+            => source.Select(header).AsParameters();
 
-        public static IList<IParameter> ToParameters(this IField source, IEnumerable<string> header)
-            => source.Select(header).ToParameters();
+        public static IList<IParameter> AsParameters(this IField source, IEnumerable<string> header)
+            => source.Select(header).AsParameters();
 
-        public static IList<IParameter> ToParameters(this ITuple tuple)
+        public static IList<IParameter> AsParameters(this ITuple tuple)
             => new ParameterStore().Add(tuple);
 
-        public static IList<IParameter> ToParameters(this IRelation relation)
+        public static IList<IParameter> AsParameters(this IRelation relation)
             => new ParameterStore().Add(relation);
 
-        public static IParameter ToParameter(this IField field, string parameterName)
+        public static IParameter AsParameter(this IField field, string parameterName)
             => new Parameter(parameterName, field);
     }
 }
